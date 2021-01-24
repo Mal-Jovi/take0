4ca97f4087103c296672e73a3d3d89ace90d5494
claraj <clarajchu@gmail.com>
2021-01-23 23:44:50 -0500

    added pics

--- IdeaProjects/experiment1/src/components/About.js
+++ IdeaProjects/experiment1/src/components/About.js
@@ -1,75 +1,87 @@
 import React from 'react';
 import "../styles/About.css";
 import {FaLinkedinIn, FaGithub, FaPinterest} from 'react-icons/fa';
+import { IoDownloadOutline } from "react-icons/io5";
 
 function About() {
   return (
-  <div className="content-container">
+<div className="content-container">
 
-  <div className="section-container" id="about">
-    <div className="section-title">hi, i'm clara ☕</div>
-    <div className="section-text">
-      zoomer student & coffee chugger. i'm obsessed with honeydew-flavoured candy and collecting a rainbow of Lindt chocolate wrappers
-      <br></br>
-      <br></br>
-      virtual globaltrotter currently residing between Toronto and Montreal. i study cs and economics at McGill university, expecting graduation summer 2021
-      <br></br>
-      <br></br>
-      <div className="pink-highlight">
-        my mission is to create wonderful useful tools that draw out opportunities for people around the globe
-      </div>
-      <br></br><br></br>
-    </div>
-  </div>
-
-  <div className="section-container-dark" id="vision">
-    <div className="section-title">vision 🚀</div>
-      <div className="section-text-dark">
-        how would i describe myself?
-        <br></br>
-        <li>dynamic</li>
-        <li>team builder</li>
-        <li>reliable</li>
-        <br></br>
-        i'm fascinated by the intersection between economics, technology, and art.
-        three industries that are constantly morphing and continuously converging.
-        i believe the time we have is precious, and that the best use of it is by uplifting our communities.. one line of code at a time!
-        <br></br><br></br>
-        <div className="blue-highlight">
-          my expertise includes business intelligence and full stack software development, but i also freelance in art & design
+    <div className="section-container" id="about">
+        <div className="section-title">hi, i'm clara ☕</div>
+        <div className="section-combine">
+            <div className="section-text">
+                zoomer student & coffee chugger. i'm obsessed with honeydew-flavoured candy and collecting a rainbow of Lindt chocolate wrappers
+                <br></br>
+                <br></br>
+                virtual globaltrotter currently residing between Toronto and Montreal. i study cs and economics at McGill university, expecting graduation summer 2021
+                <br></br>
+                <br></br>
+                <div className="pink-highlight">
+                    my mission is to create wonderful useful tools that draw out opportunities for people around the globe
+                </div>
+                <br></br><br></br>
+            </div>
+            <div className="propic"></div>
         </div>
-                <a href="https://drive.google.com/file/d/1jrO52kN-ihz59b82jjJZuOkwe2MOU44b/view?usp=sharing" target="_blank">resume</a>
-        <br></br><br></br>
-      </div>
     </div>
+
+
+    <div className="section-container" id="vision">
+        <div className="section-combine">
+        <div className="section-title-dark">vision 🚀</div>
+            <div className="section-text-dark">
+                dynamic, team builder, reliable
+                <br></br><br></br>
+                i'm fascinated by the intersection between economics, technology, and art.
+                three industries that are constantly morphing and continuously converging.
+                i believe the time we have is precious, and that the best use of it is by uplifting our communities.. one line of code at a time!
+                <br></br><br></br>
+                <div className="blue-highlight">
+                    my expertise includes business intelligence and full stack software development, but i also freelance in art & design
+                </div>
+                <br></br><br></br>
+            </div>
+            <div className="resume">
+                <a href="https://drive.google.com/file/d/1jrO52kN-ihz59b82jjJZuOkwe2MOU44b/view?usp=sharing" target="_blank" rel="noreferrer">
+                    <IoDownloadOutline size="2.5em"/>
+                </a>
+            </div>
+        </div>
+    </div>
+
 
     <div className="section-container" id="contact">
-      <div className="section-title">let's chat 📝</div>
-      <div className="section-text">
-        i'm always on the lookout for a new book recommendation, an underground indie band, or just to bounce around some ideas! let's get connected below
-        <br></br><br></br>
-        <div className="orange-highlight">
-          exploring three.js has been so much fun, and i'd love to collaborate on the next UI/UX project together
+        <div className="section-title">let's chat 📝</div>
+        <div className="section-text">
+            i'm always on the lookout for a new book recommendation, an underground indie band, or just to bounce around some ideas! let's get connected below
+            <br></br><br></br>
+            <div className="orange-highlight">
+                exploring three.js has been so much fun, and i'd love to collaborate on the next UI/UX project together
+            </div>
+            <br></br><br></br>
+            <div className="icon">
+                <a href="https://www.linkedin.com/in/clarajchu/" target="_blank" rel="noreferrer">
+                    <FaLinkedinIn size="3em"/>
+                </a>
+                <a href="https://github.com/armydillo0/take0/tree/main/IdeaProjects/experiment1" target="_blank" rel="noreferrer">
+                    <FaGithub size="3em"/>
+                </a>
+                <a href="https://www.pinterest.ca/clarajchu" target="_blank" rel="noreferrer">
+                    <FaPinterest size="3em"/>
+                </a>
+            </div>
+            <br></br><br></br>
         </div>
-        <br></br><br></br>
-
-        <div className="icon">
-          <a href="https://www.linkedin.com/in/clarajchu/" target="_blank"><FaLinkedinIn size="3em"/></a>
-          <a href="https://github.com/armydillo0/take0/tree/main/IdeaProjects/experiment1" target="_blank"><FaGithub size="3em"/></a>
-          <a href="https://www.pinterest.ca/clarajchu" target="_blank"><FaPinterest size="3em"/></a>
-        </div>
-
-        <br></br><br></br>
-      </div>
     </div>
+
 
     <div className="play-container" id="play">
-      <br></br><br></br>
-      <div className="play-game"></div>
-      <div className="play-text">3d objects i made to practise texture & lighting</div>
+        <br></br><br></br>
+        <div className="play-game"></div>
+        <div className="play-text">3d objects i made to practise texture & lighting</div>
     </div>
 
-  </div>
-  );
+</div>  );
 }
 export default About;
--- IdeaProjects/experiment1/src/components/Navigation.js
+++ IdeaProjects/experiment1/src/components/Navigation.js
@@ -5,13 +5,13 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 function Navigation() {
   return (
-    <div className="navbar">
-        <Link to="/#about" smooth={true} duration={3500}>about</Link>
-        <Link to="/#vision" smooth={true} duration={3500}>vision</Link>
-        <li><GiAbstract082 size="1.75em"/></li>
-        <Link to="/#contact" smooth={true} duration={3500}>contact</Link>
-        <Link to="/#play" smooth={true} duration={3500}>play</Link>
-    </div>
+<div className="navbar">
+    <Link to="/#about" smooth={true} duration={3500}>about</Link>
+    <Link to="/#vision" smooth={true} duration={3500}>vision</Link>
+    <li><GiAbstract082 size="1.75em"/></li>
+    <Link to="/#contact" smooth={true} duration={3500}>contact</Link>
+    <Link to="/#play" smooth={true} duration={3500}>play</Link>
+</div>
   );
 }
 export default Navigation;
